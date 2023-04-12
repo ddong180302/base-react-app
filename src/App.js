1d62e8408e1f47bfa654a60481c9fe4344d30682
{ddong180302} <{trandangdong18032002@gmail.com}>
2023-04-12 14:09:28 +0700

    #17

--- src/App.js
+++ src/App.js
@@ -3,24 +3,49 @@ import './App.css';
 import MyComponent from './components/MyComponents';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+import React from 'react';
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
-      <MyComponent />
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    name: 'dong',
+    address: 'da nang',
+    age: 21
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        my name
+        {
+          this.state.name
+        }
+        <MyComponent />
+      </div>
+    );
+  }
 }
 
+
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   console.log('check state: ', useSelector(state => state.counter))
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//       <MyComponent />
+//     </div>
+//   );
+// }
 export default App;
