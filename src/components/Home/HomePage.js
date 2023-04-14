41db9ad1fad4a29f28cdf16781ec8c6535e5ef41
{ddong180302} <{trandangdong18032002@gmail.com}>
2023-04-15 05:39:59 +0700

    #53

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -6,6 +6,15 @@ const HomePage = (props) => {
             <video loop autoPlay muted>
                 <source src={videoHomepage} type="video/mp4" />
             </video>
+            <div className='homepage-content'>
+                <div className='title-one'>There's a better way to ask</div>
+                <div className='title-two'>You don't want to make a boring form.
+                    And your audience won't answer one.
+                    Create a typeform instead—and make everyone happy.</div>
+                <div className='title-three'>
+                    <button>Get started - it's free</button>
+                </div>
+            </div>
         </div>
     )
 }
