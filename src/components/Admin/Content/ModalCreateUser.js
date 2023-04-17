a53cb5a0a0f76dbc486265e57853f217fc300380
{ddong180302} <{trandangdong18032002@gmail.com}>
2023-04-17 14:12:00 +0700

    #59

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -3,6 +3,8 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { FcPlus } from 'react-icons/fc';
 import axios from 'axios';
+import { toast } from 'react-toastify';
+
 
 const ModalCreateUser = (props) => {
     const { show, setShow } = props;
@@ -34,19 +36,45 @@ const ModalCreateUser = (props) => {
         // }
         console.log('upload file', event.target.files[0])
     }
+    const validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
 
     const handleSubmitCreateUser = async () => {
 
         //validate
+        const isValidEmail = validateEmail(email);
+        if (!isValidEmail) {
+            toast.error('Invalid email');
+            return;
+        }
 
-        //call apis
-        // let data = {
-        //     email: email,
-        //     password: password,
-        //     username: username,
-        //     role: role,
-        //     image: image,
-        // }
+        if (!password) {
+            toast.error('Invalid password');
+            return;
+        }
+
+        if (!username) {
+            toast.error('Invalid username');
+            return;
+        }
+
+        if (!role) {
+            toast.error('Invalid role');
+            return;
+        }
+
+        if (!image) {
+            toast.error('Invalid image');
+            return;
+        }
+
+        //submit data
+
         // console.log("data: ", data)
         const data = new FormData();
         data.append('email', email);
@@ -59,6 +87,14 @@ const ModalCreateUser = (props) => {
 
         let res = await axios.post('http://localhost:8800/api/v1/participant', data)
         console.log('check res: ', res);
+        if (res.data && res.data.errCode === 0) {
+            toast.success('Create a new user succeed!')
+            handleClose();
+        }
+
+        if (res.data && res.data.errCode !== 0) {
+            toast(res.data.errMessage);
+        }
     }
 
     return (
