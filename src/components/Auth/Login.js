5d62f8ea6ea4a4355a881927e4faf82a7bce136d
{ddong180302} <{trandangdong18032002@gmail.com}>
2023-04-25 14:15:31 +0700

    #69

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,9 +1,56 @@
+import { useState } from 'react';
+import './Login.scss';
+
+
 
 
 const Login = (props) => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        alert('login')
+    }
+
     return (
-        <div>
-            login component
+        <div className="login-container">
+            <div className='header'>
+                Don't have an account yet?
+            </div>
+            <div className='title col-4 mx-auto' >
+                Tran Dang Dong
+            </div>
+            <div className='welcome col-4 mx-auto'>
+                Hello, who's this?
+            </div>
+            <div className='content-form col-4 mx-auto'>
+                <div className='form-group'>
+                    <lable>Email</lable>
+                    <input
+                        type='email'
+                        className='form-control'
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
+                </div>
+                <div className='form-group'>
+                    <lable>PassWord</lable>
+                    <input
+                        type='password'
+                        className='form-control'
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                </div>
+                <span className='forgot-password'>Forgot password ?</span>
+                <div>
+                    <button
+                        className='btn-submit'
+                        onClick={() => handleLogin()}
+                    >Login to Quiz</button>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,15 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Outlet, NavLink } from "react-router-dom";
+import { Outlet, NavLink, useNavigate } from "react-router-dom";
 
 const Header = () => {
+    const navigate = useNavigate();
+
+    const handleLogin = () => {
+        navigate('/login')
+    }
+
     return (
         <Navbar bg="light" expand="lg">
             <Container>
@@ -17,7 +23,7 @@ const Header = () => {
                         <NavLink className='nav-link' to="/admins">Admin</NavLink>
                     </Nav>
                     <Nav>
-                        <button className='btn-login'>Log in</button>
+                        <button className='btn-login' onClick={() => handleLogin()}>Log in</button>
                         <button className='btn-signup'>Sign up</button>
                         {/* <NavDropdown title="Setting" id="basic-nav-dropdown">
                             <NavDropdown.Item>Log in</NavDropdown.Item>
