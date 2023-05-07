9abfcca5d5d0c72cc25c68b03772ef2e614b2640
{ddong180302} <{trandangdong18032002@gmail.com}>
2023-05-08 03:42:51 +0700

    #83

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiServices";
+import _ from 'lodash';
+
+
 const DetailQuiz = (props) => {
     const params = useParams();
     const quizId = params.id;
@@ -11,7 +14,32 @@ const DetailQuiz = (props) => {
 
     const fetchQuestions = async () => {
         let res = await getDataQuiz(quizId);
-        console.log('check question: ', res);
+        console.log('check question: ', res.data.errCode);
+
+        if (res && res.data && res.data.errCode === 0) {
+            let raw = res.data.data;
+            let data = _.chain(raw)
+                .groupBy("id")
+                .map((value, key) => {
+                    //let temp = 
+                    let answers = [];
+                    let questionDescription, image = null;
+                    value.forEach((item, index) => {
+                        if (index === 0) {
+                            questionDescription = item.description;
+                            image = item.image;
+                        }
+                        answers.push(item.answerData)
+                        console.log('item: ', item.answerData)
+                    })
+
+                    return {
+                        questionId: key, answers, questionDescription, image
+                    }
+                })
+                .value();
+            console.log('data tda: ', data)
+        }
     }
     console.log("check params: ", params)
     return (
